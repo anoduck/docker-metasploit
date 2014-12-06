3e97f41c5e0ec56cd3d81faa6e86291847456477
Paul Liljenberg <paul@Pauls-MacBook-Pro.local>
2014-12-06 11:46:04 +0100

    fix connection issues to postgres db

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-MSFPASS=${MSFPASS:-msf}
+MSFUSER=${MSFUSER:-postgres}
+MSFPASS=${MSFPASS:-postgres}
 if [[ ! -z "$DB_PORT_5432_TCP_ADDR" ]]; then
   # Check if user exists
-  USEREXIST="$(psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='msf'")"
+  USEREXIST="$(psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$MSFUSER'")"
   # If not create it
-  if [[ ! $MSFEXIST -eq 1 ]]; then
-	 psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -c "create role msf login password '$MSFPASS'"
+  if [[ ! $USEREXIST -eq 1 ]]; then
+	 psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -c "create role $MSFUSER login password '$MSFPASS'"
   fi
 
   DBEXIST="$(psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres  postgres -l | grep msf)"
   if [[ ! $DBEXIST ]]; then
-	 psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -c "CREATE DATABASE msf OWNER msf;"
+	 psql -h $DB_PORT_5432_TCP_ADDR -p 5432 -U postgres postgres -c "CREATE DATABASE msf OWNER $MSFUSER;"
   fi
 
 sh -c "echo 'production:
   adapter: postgresql
   database: msf
-  username: msf
+  username: $MSFUSER
   password: $MSFPASS
   host: $DB_PORT_5432_TCP_ADDR
   port: 5432
